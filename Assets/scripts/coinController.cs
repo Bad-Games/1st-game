2fff68b50b5577acb8371db35c8ca2c26bf679b2
Unknown <daaraquem@unal.edu.co>
2019-01-10 16:20:15 -0500

    Base
    Contiene los aspectos básicos y el primer sprite

---
+++ Assets/scripts/coinController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinController : MonoBehaviour
+{
+    
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        scoreManager.ScoreManager.RaiseScore(1);
+        Destroy(transform.parent.gameObject);
+    }
+}
---
+++ Assets/scripts/scoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scoreManager : MonoBehaviour
+{   
+    public static scoreManager ScoreManager;
+
+    public Text scoreText;
+    int score = 0;
+
+    void Start()
+    {
+        if(ScoreManager == null)
+        {
+            ScoreManager = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    void Update()
+    {
+        if(scoreText == null)
+        {
+            scoreText = GameObject.Find("Text").GetComponent<Text>();
+            scoreText.text = score + "";
+        }   
+    }
+
+    public void RaiseScore(int s)
+    {
+        score += s;
+        scoreText.text = score + "";
+
+        if (score == 3)
+        {
+            SceneManager.LoadScene("scene2");
+        }
+    }
+}
